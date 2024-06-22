4b1066cd14f1858014c284bcab8368d477def49c
cimisachimi <72540063+cimisachimi@users.noreply.github.com>
2024-06-22 13:46:18 +0700

    Minggu 22/jun/24

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,35 +4,41 @@ import profilePic from "../assets/profile.png";
 
 const Navbar = () => {
   return (
-    <header className="w-full text-gray-700 bg-creamBase border-t border-gray-100 shadow-sm body-font border-b-darkCream">
+    <nav className="w-full text-gray-700 bg-creamBase border-t border-gray-100 shadow-sm body-font border-b-darkCream">
       <div className="container flex flex-col md:flex-row items-center justify-between p-5 mx-auto">
         {/* Left Section - Logo */}
-        <div className="flex items-center">
+        <div className="flex items-center justify-start flex-grow md:flex-grow-0">
           <img
             src={logo}
             alt="Logo"
             className="h-auto max-h-14 md:max-h-16 lg:max-h-20"
+            height={150}
+            width={150}
           />
         </div>
 
-        {/* Center Section - Big Text */}
-        <div className="flex items-center justify-center flex-grow">
-          <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-gray-900">
-            Panel Admin CampStore
-          </h1>
+        {/* Center Section - Menu Links and Search */}
+        <div className="flex items-center justify-center flex-grow space-x-4">
+          <h1 className="text-darkCream">Panel Admin </h1>
         </div>
 
-        {/* Right Section - Profile Picture and Name */}
-        <div className="flex items-center">
+        {/* Right Section - Profile Picture, Name, and Icons */}
+        <div className="flex items-center justify-end flex-grow md:flex-grow-0 space-x-4">
+          <span className="font-medium text-gray-900">Chimi</span>
           <img
             src={profilePic}
             alt="Profile"
             className="w-10 h-10 rounded-full mr-3"
           />
-          <span className="font-medium text-gray-900">Chimi</span>
+          <a href="#" className="text-gray-900 hover:text-gray-700">
+            <i className="fas fa-bell"></i>
+          </a>
+          <a href="#" className="text-gray-900 hover:text-gray-700">
+            <i className="fas fa-cog"></i>
+          </a>
         </div>
       </div>
-    </header>
+    </nav>
   );
 };
 
